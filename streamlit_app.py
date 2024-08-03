6cfff942c64575e161bd12ba670304b74aeda40e
Tanushree <tanugadalay@gmail.com>
2024-08-03 14:26:44 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,5 +4,6 @@ st.title("🎈 My new app")
 st.write("Hello world")
 x = st.text_input("favorate movie")
 st.write(f"You favorate movie is : {x}")
+is_clicked = st.button("click here")
 
               
