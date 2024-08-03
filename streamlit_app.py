c95351fcbfb223cb347e9cac75644e1bc612146c
Tanushree <tanugadalay@gmail.com>
2024-08-03 12:37:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.write("Hello world")
 st.text_input("favorate movie)
               
